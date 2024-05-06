43793c8326e2d9296fde7f5dbcef5e03f3a160a1
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-05-06 15:14:50 +0500

    create markdown custom component

---
+++ components/Markdown.tsx
@@ -0,0 +1,9 @@
+import ReactMarkdown from "react-markdown";
+
+export default function Markdown(props:{markdown:string}){
+    const defaultMarkdown = `#**Plasmic *CustomComponents*!!!!`;
+
+    return  (
+        'Markdown Markdown Smth anything'
+    )
+}
